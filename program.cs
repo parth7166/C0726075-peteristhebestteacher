ee83d0023f4eee29529c5bf889c9e6fd7cd9e86f
parth7166 <46794689+parth7166@users.noreply.github.com>
2019-04-04 13:29:23 -0400

    Add files via upload

---
+++ program.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0726075
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var dogs = new List<string>();
+            dogs.Add("Bichon");
+            dogs.Add("Poodle");
+            dogs.Add("Retriever");
+           foreach (var dog in dogs)
+           {
+                Console.WriteLine(dog + "");
+               
+            }
+            Console.WriteLine(dogs[1]);
+
+            int[] a1 = new int[] { 1, 3, 5, 7, 9, 11 };
+            foreach (int i in a1)
+            {
+                Console.WriteLine(i + "");
+
+            }Console.ReadLine();
+        }
+    }
+}
